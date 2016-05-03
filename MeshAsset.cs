a88849021dbbcf2e62f44d7f738b99b61831e272
unknown <yevhen.brusilovskyy@yevhen-brusilo1.office.innogames.net>
2016-05-03 15:08:34 +0200

    Class for mesh data and loader class added

---
+++ MeshAsset.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace AssetTool
+{
+    class MeshAsset
+    {
+        public struct Vertex
+        {
+            public int Point, TexCoord, Normal;
+        }
+
+        private struct Vec2D
+        {
+            public float X, Y;
+            public Vec2D(float x, float y) { X = x; Y = y; }
+        }
+
+        private struct Vec3D
+        {
+            public float X, Y, Z;
+            public Vec3D(float x, float y, float z) { X = x; Y = y; Z = z; }   
+        }
+
+        private struct Triangle
+        {
+            public Vertex VertexA, VertexB, VertexC;
+            public Triangle(ref Vertex a, ref Vertex b, ref Vertex c) { VertexA = a; VertexB = b; VertexC = c; }
+        }
+
+        private List<Vec3D> Points = null;
+        private List<Vec3D> Normals = null;
+        private List<Vec2D> TexCoords = null;
+        private List<Triangle> Triangles = null;
+
+        public void AddTriangle(ref Vertex a, ref Vertex b, ref Vertex c)
+        {
+            if (Triangles == null)
+            {
+                Triangles = new List<Triangle>();
+            }
+            Triangles.Add(new Triangle(ref a, ref b, ref c));
+        }
+
+        public void AddPoint(float x, float y, float z)
+        {
+            if (Points == null)
+            {
+                Points = new List<Vec3D>();      
+            }
+            Points.Add(new Vec3D(x, y, z));
+        }
+
+        public void AddNormal(float x, float y, float z)
+        {
+            if (Normals == null)
+            {
+                Normals = new List<Vec3D>();
+            }
+            Normals.Add(new Vec3D(x, y, z));
+        }
+
+        public void AddTexCoord(float x, float y)
+        {
+            if (TexCoords == null)
+            {
+                TexCoords = new List<Vec2D>();
+            }
+            TexCoords.Add(new Vec2D(x, y));
+        }
+
+    }
+}
---
+++ MeshLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace AssetTool
+{
+    class MeshLoader
+    {
+        public static MeshAsset LoadFromOBJ(StreamReader source)
+        {
+            if (source != null)
+            {
+                string line = null;
+                MeshAsset asset = new MeshAsset();
+                while ((line = source.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(' ');
+                    switch (parts[0].ToLower())
+                    {
+                        case "v":
+                            asset.AddPoint(float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3]));
+                            break;
+                        case "vn":
+                            asset.AddNormal(float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3]));
+                            break;
+                        case "vt":
+                            asset.AddTexCoord(float.Parse(parts[1]), float.Parse(parts[2]));
+                            break;
+                        case "f":
+                            break;
+                    }
+                }
+                return asset;
+            }
+            return null;
+        }
+    }
+}
