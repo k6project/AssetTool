26c41df8027ed048c781543fc79e194c39ea25e6
unknown <yevhen.brusilovskyy@yevhen-brusilo1.office.innogames.net>
2016-05-03 15:12:48 +0200

    Basic startup routine implemented for testing

--- AssetTool.cs
+++ AssetTool.cs
@@ -1,9 +1,18 @@
-﻿namespace AssetTool
+﻿using System.IO;
+
+namespace AssetTool
 {
     class AssetTool
     {
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                using (StreamReader source = new StreamReader(args[0]))
+                {
+                    MeshAsset asset = MeshLoader.LoadFromOBJ(source);
+                }
+            }
         }
     }
 }
